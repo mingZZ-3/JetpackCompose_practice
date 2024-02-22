a1eb67bbc885b67f3e7896dffd6057920a0a8494
Minji <alswl7754@gmail.com>
2024-02-22 23:13:05 +0900

    Create project and link with GitHub

---
+++ app/src/main/java/com/pfdy/jetpackcompose_practice/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.pfdy.jetpackcompose_practice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pfdy.jetpackcompose_practice.ui.theme.JetpackCompose_practiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackCompose_practiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackCompose_practiceTheme {
+        Greeting("Android")
+    }
+}
